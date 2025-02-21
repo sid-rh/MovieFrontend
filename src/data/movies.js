e32b0c2bbae73fc23e074e750c9e81cdd68703ca
sid-rh <siddhanth.rh@gmail.com>
2025-02-21 09:57:01 +0530

    commit

--- src/data/movies.js
+++ src/data/movies.js
@@ -3,13 +3,13 @@ export const movies = [
     id: 1,
     name: "The Godfather",
     year: 1972,
-    image: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+    image: "https://m.media-amazon.com/images/I/61k7Mx2IjzL._SL1100_.jpg"
   },
   {
     id: 2,
     name: "Inception",
     year: 2010,
-    image: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg"
+    image: "https://m.media-amazon.com/images/I/71DwIcSgFcS.jpg"
   },
   {
     id: 3,
@@ -21,18 +21,18 @@ export const movies = [
     id: 4,
     name: "The Menu",
     year: 2022,
-    image: "https://m.media-amazon.com/images/M/MV5BMzdjNjI2MGItN2JhNS00MTc5LTg5YzAtYTRkNGUyYjFkNDU0XkEyXkFqcGdeQXVyMTAyMjQ3NzQ1._V1_.jpg"
+    image: "https://artofthemovies.co.uk/cdn/shop/products/IMG_0671_1024x1024@2x.jpg?v=1673179623"
   },
   {
     id: 5,
     name: "Get Out",
     year: 2017,
-    image: "https://m.media-amazon.com/images/M/MV5BMjUxMDQwNjcyNl5BMl5BanBnXkFtZTgwNzcwMzc0MTI@._V1_.jpg"
+    image: "https://artofthemovies.co.uk/cdn/shop/files/IMG_6918_720x@2x.jpg?v=1715961921"
   },
   {
     id: 6,
     name: "National Treasure",
     year: 2004,
-    image: "https://m.media-amazon.com/images/M/MV5BMTY3NTc4OTYxMF5BMl5BanBnXkFtZTcwMjk5NzUyMw@@._V1_.jpg"
+    image: "https://image.tmdb.org/t/p/original/pxL6E4GBOPUG6CdkO9cUQN5VMwI.jpg"
   }
 ];
